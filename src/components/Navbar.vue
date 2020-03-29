566c0a3603579c13dbf8260a38773fc3299071e2
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 02:42:25 +0530

    chore: made navbar sticky

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="text-white font-bold text-4xl flex flex-row bg-primary py-5">
-    <div class="w-1/12 text-center">Menu</div>
-    <div class="w-10/12 text-center">Bang Bang Bakery</div>
-    <div class="w-1/12 text-center">Cart</div>
+  <div
+    class="text-white font-bold text-2xl flex flex-row bg-primary sticky top-0 object-contain py-2"
+  >
+    <div class="text-center">Menu</div>
+    <div class="w-full text-center">Bang Bang Bakery</div>
+    <div class="text-center">Cart</div>
   </div>
 </template>
 
