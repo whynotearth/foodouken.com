7c6ba14d3393eaf16aa40b42c6d5c885ce3dd205
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 02:42:06 +0530

    chore: avatar

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="text-center">
-    <div>
-      <img src="../assets/logo.png" alt="Logo image" class="h-48 w-48 m-auto" />
+    <div class="mt-8">
+      <img
+        src="https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg"
+        alt="Logo image"
+        class="h-48 w-48 m-auto rounded-full"
+      />
     </div>
     <h1 class="text-white text-6xl font-semibold">Bang Bang Bakery-Café</h1>
     <h2 class="text-5xl text-gray-500 font-bold">Delivery</h2>
