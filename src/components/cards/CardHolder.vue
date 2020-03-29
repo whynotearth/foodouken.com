b23fa8b0d8d90eb4c0f5ed45e3ec66a66c1ab294
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 00:20:43 +0530

    chore: created cardholder and setup home.vue

---
+++ src/components/cards/CardHolder.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <category-card />
+  </div>
+</template>
+
+<script>
+import CategoryCard from '@/components/cards/CategoryCard.vue';
+
+export default {
+  name: 'CardHolder',
+  components: {
+    CategoryCard
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <hero-section />
+    <hr class="my-8 border-gray-700" />
+    <section class="flex flex-row my-4">
+      <div class="w-8/12 border-r border-gray-700">
+        <card-holder />
+      </div>
+      <div class="w-4/12 "></div>
+    </section>
+  </div>
 </template>
 
 <script>
+import HeroSection from '@/components/HeroSection.vue';
+import CardHolder from '@/components/cards/CardHolder.vue';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HeroSection,
+    CardHolder
+  }
 };
 </script>
