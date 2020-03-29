a851b8107bccadca4a97c373d769afb5aef072ab
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 00:18:27 +0530

    feat: setup app.vue and added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="app">
+  <div id="app" class="font-sans bg-primary min-h-screen px-5 pb-5">
+    <Navbar />
     <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-white font-bold text-4xl flex flex-row bg-primary py-5">
+    <div class="w-1/12 text-center">Menu</div>
+    <div class="w-10/12 text-center">Bang Bang Bakery</div>
+    <div class="w-1/12 text-center">Cart</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
