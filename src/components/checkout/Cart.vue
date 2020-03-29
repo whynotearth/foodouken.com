3e6d985674f461f0e9f004da0163928cab12dc9f
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 02:44:19 +0530

    feat: cart setup

---
+++ src/components/checkout/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-secondary shadow-md rounded-lg mb-4 p-4">
+    <cart-item v-for="x in 2" :key="x" />
+    <hr class="my-4" />
+    <span class="text-gray-500">Sub total</span>
+    <br />
+    <span class="text-gray-500">Delivery fee</span>
+    <br />
+    <span class="text-gray-400 text-2xl font-bold">Total</span>
+  </div>
+</template>
+
+<script>
+import CartItem from '@/components/checkout/CartItem.vue';
+
+export default {
+  name: 'Cart',
+  components: {
+    CartItem
+  }
+};
+</script>
+
+<style></style>
