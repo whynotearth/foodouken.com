7ac470c81904dfdc28a295aadc127c24435703b4
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 00:20:10 +0530

    feat: created category card

---
+++ src/components/cards/CategoryCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-secondary shadow-md rounded-lg h-48 w-1/2 md:mr-4">
+    <img src="" alt="Category image" class="w-auto h-40" />
+    <span class="p-3 text-white font-semibold">Category name</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryCard'
+};
+</script>
+
+<style></style>
