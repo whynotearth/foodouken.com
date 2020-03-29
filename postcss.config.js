8df2c07379bc508d7b9bd4ad25b9453bbbaa881c
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-29 19:51:25 +0530

    feat: setup tailwind, pug and prettier config

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+const postcssPurgecss = require(`@fullhuman/postcss-purgecss`);
+
+const purgecss = postcssPurgecss({
+  content: ['./public/**/*.html', './src/**/*.vue'],
+  defaultExtractor: content => {
+    const contentWithoutStyleBlocks = content.replace(
+      /<style[^]+?<\/style>/gi,
+      ''
+    );
+    return (
+      contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+    );
+  },
+  whitelistPatterns: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!cursor-move).+-move$/,
+    /^router-link(|-exact)-active$/
+  ]
+});
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
+};
