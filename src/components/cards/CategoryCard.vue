2b6e4fffab5fe3fe2cdb5a275d361d3d343a3350
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 02:43:04 +0530

    chore: category card additions

--- src/components/cards/CategoryCard.vue
+++ src/components/cards/CategoryCard.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="bg-secondary shadow-md rounded-lg h-48 w-1/2 md:mr-4">
-    <img src="" alt="Category image" class="w-auto h-40" />
-    <span class="p-3 text-white font-semibold">Category name</span>
+  <div class="bg-secondary shadow-md rounded-lg h-48 mb-4">
+    <img
+      src="https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg"
+      alt="Category image"
+      class="w-auto h-40 w-full rounded-t-lg"
+    />
+    <span class="p-4 text-white font-semibold">{{ title }}</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CategoryCard'
+  name: 'CategoryCard',
+  props: {
+    title: {
+      type: String,
+      default: 'Category name'
+    },
+    image: {
+      type: URL
+    }
+  }
 };
 </script>
 
