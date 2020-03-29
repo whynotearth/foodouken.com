29faedb1d3a7786cb4db6f44cc2ad3ce7232efa3
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 00:14:38 +0530

    feat: hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-center">
+    <div>
+      <img src="../assets/logo.png" alt="Logo image" class="h-48 w-48 m-auto" />
+    </div>
+    <h1 class="text-white text-6xl font-semibold">Bang Bang Bakery-Café</h1>
+    <h2 class="text-5xl text-gray-500 font-bold">Delivery</h2>
+    <p class="text-gray-500 mt-2">
+      🍞 Small #artisan bakery and café in #SiemReap
+      <br />
+      🍪 Visit us Wed - Sun 8am - 6pm
+      <br />
+      🥨 Sustainable 🤝🏼 Responsible 🌏 Eco-friendly
+      <br />
+      🇰🇭 Proudly Cambodian owned 🇰🇭
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroSection'
+};
+</script>
+
+<style></style>
