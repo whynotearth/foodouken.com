d7b98ad28aa82dc5a81901a99dcb263306903b43
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-30 21:43:17 +0530

    feat: new category holder

---
+++ src/components/categories/CategoryHolder.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="disable-scrollbars overflow-x-auto scrolling-touch flex flex-no-wrap mb-4"
+  >
+    <category
+      v-for="category in categories"
+      :key="category.id"
+      @clicked="selectCategory(category)"
+      :selected="selectedCategory === category"
+      :title="category.name"
+    />
+  </div>
+</template>
+
+<script>
+import Category from '@/components/categories/Category.vue';
+import data from '@/db/data.json';
+
+export default {
+  name: 'CategoryHolder',
+  components: {
+    Category
+  },
+  data() {
+    return {
+      categories: data.categories,
+      selectedCategory: {}
+    };
+  },
+  methods: {
+    selectCategory(category) {
+      this.selectedCategory = category;
+      this.$router.push(`/${category.slug}`);
+    }
+  }
+};
+</script>
+
+<style>
+.disable-scrollbars::-webkit-scrollbar {
+  width: 0px;
+  background: transparent; /* Chrome/Safari/Webkit */
+}
+
+.disable-scrollbars {
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE 10+ */
+}
+</style>
