f22ced3037118a3a751f861ad366066a87233fba
Atharva Sharma <mailatharvasharma@gmail.com>
2020-03-31 12:36:34 +0530

    feat: added number input

--- src/components/checkout/CartItem.vue
+++ src/components/checkout/CartItem.vue
@@ -3,20 +3,33 @@
     <img
       src="https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466__340.jpg"
       class="h-12 w-12"
-      alt="item image"
+      alt="Product image"
     />
     <div class="w-full mx-2">
       <span class="text-gray-400">{{ trimmedTitle }} </span>
       <br />
       <span class="text-gray-500">${{ price }}</span>
     </div>
-    <div>
+    <div class="flex text-gray-500 font-bold text-lg">
+      <div
+        class="border-2 border-gray-500 rounded-full m-auto p-0 h-8 w-8 text-center cursor-pointer select-none"
+        @click="decrement"
+      >
+        -
+      </div>
       <input
         type="number"
         name="quantity"
+        min="1"
         v-model="quantity"
-        class="bg-transparent w-10 align-middle text-gray-500"
+        class="bg-transparent w-8 py-3 text-center"
       />
+      <div
+        class="border-2 border-gray-500 rounded-full m-auto p-0 h-8 w-8 text-center cursor-pointer select-none"
+        @click="increment"
+      >
+        +
+      </div>
     </div>
   </div>
 </template>
@@ -30,8 +43,34 @@ export default {
       price: 8,
       quantity: 1
     };
+  },
+  methods: {
+    increment() {
+      if (this.quantity >= 10) {
+        return;
+      }
+      this.quantity++;
+    },
+    decrement() {
+      if (this.quantity <= 1) {
+        return;
+      }
+      this.quantity--;
+    }
   }
 };
 </script>
 
-<style></style>
+<style>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+</style>
