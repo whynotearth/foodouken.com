67b2da8b6599cdbe4f8ea510d36fe84a8ae88739
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-01 17:21:25 +0530

    chore: added smooth scroll and fixed width

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <div id="app" class="font-sans bg-primary h-full max-w-screen-xl">
+  <div id="app" class="font-sans bg-primary h-full">
     <Navbar />
-    <router-view class="md:mx-8 pb-5 mx-4" />
+    <router-view
+      class="md:px-8 pb-8 px-4 max-w-screen-xxl mx-auto min-h-screen"
+    />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 };
 </script>
 
 <style>
+html,
+body {
+  scroll-behavior: smooth;
+}
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
