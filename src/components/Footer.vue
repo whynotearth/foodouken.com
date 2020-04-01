fe8ae3a0f10bcc8355fd1c2884a2a13b099d1ab6
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-01 17:19:50 +0530

    add: footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="h-full bg-footer text-lg text-white static">
+    <div
+      class="max-w-screen-xxl flex md:flex-row flex-col justify-between px-4 md:px-8 py-12 mx-auto"
+    >
+      <span>Taphul Rd, Opposite Idea Decor</span>
+      <span>098 976 987</span>
+      <span>/BakeryBangBang</span>
+      <span>097 947 7799</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
