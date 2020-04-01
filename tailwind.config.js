3a89c73ee00f0340d6aaf5ade20d11af95a22757
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-01 17:12:51 +0530

    chore: refactored tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,35 +4,13 @@ module.exports = {
       colors: {
         primary: '#003037',
         secondary: '#173F44',
-        tertiary: '#0E9EC2'
+        button: '#0E9EC2',
+        footer: '#437784'
+      },
+      screens: {
+        xl: '1280px',
+        xxl: '1920px'
       }
-      // screens: {
-      //   sm: '640px',
-      //   md: '768px',
-      //   lg: '1024px',
-      //   xl: '1280px',
-      //   dark: { raw: '(prefers-color-scheme: dark)' },
-      //   light: { raw: '(prefers-color-scheme: light)' }
-      // },
-      // colors: {
-      //   licorice: '#101010',
-      //   night: '#141414',
-      //   light: '#fcfcfc',
-      //   charcoal: '#545454',
-      //   black: '#000000',
-      //   white: '#FFFFFF'
-      // },
-      // fontFamily: {
-      //   display: ['Playfair Display', 'serif']
-      // },
-      // fontSize: {
-      //   '7xl': '5rem',
-      //   huge: '10rem'
-      // },
-      // boxShadow: {
-      //   '3xl-light': '0 35px 60px -15px rgba(0, 0, 0, .3)',
-      //   '3xl-dark': '0 35px 60px -15px rgba(0, 0, 0, 1)'
-      // }
     }
   },
   variants: {
