7595c7de9c0f8bf7240f5fed69dfd262553377f6
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-01 17:20:53 +0530

    chore: improved navar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div
-    class="text-white font-bold text-2xl flex flex-row bg-primary sticky top-0 object-contain p-2 md:px-8"
-  >
-    <div class="text-center">Menu</div>
-    <div class="w-full text-center">Bang Bang Bakery</div>
-    <div class="text-center">Cart</div>
+  <div class="text-white font-bold text-2xl bg-primary sticky top-0">
+    <div class="max-w-screen-xxl flex flex-row py-2 px-4 md:px-8 m-auto">
+      <div class="text-center">Menu</div>
+      <div class="w-full text-center">Bang Bang Bakery</div>
+      <div class="text-center">Cart</div>
+    </div>
   </div>
 </template>
 
