2c69be6df10f3d4f0270b87c703c80749a1a87dd
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-01 15:49:56 +0530

    chore: navbar edits

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-white font-bold text-2xl flex flex-row bg-primary sticky top-0 object-contain py-2"
+    class="text-white font-bold text-2xl flex flex-row bg-primary sticky top-0 object-contain p-2 md:px-8"
   >
     <div class="text-center">Menu</div>
     <div class="w-full text-center">Bang Bang Bakery</div>
