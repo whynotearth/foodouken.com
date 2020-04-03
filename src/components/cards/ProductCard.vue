5eefe115b40b39898dacbd47f991fded9ca39ad1
FrozenIce0617 <prosdev087@gmail.com>
2020-04-03 12:42:20 +0200

    chore: fix CSS styles

--- src/components/cards/ProductCard.vue
+++ src/components/cards/ProductCard.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="bg-secondary flex flex-row shadow-md rounded-lg h-24 mb-4">
-    <img :src="product.image" alt="Product image" class="w-3/12 h-full object-cover rounded-l-lg" />
+    <img
+      :src="product.image"
+      alt="Product image"
+      class="w-3/12 h-full object-cover rounded-l-lg"
+    />
     <p class="my-auto">
-      <span class="mx-4 text-gray-400 my-1 inline-block text-lg">{{ product.name }}</span>
+      <span class="mx-4 text-gray-400 my-1 inline-block text-lg">
+        {{ product.name }}
+      </span>
       <br />
-      <span class="mx-4 text-gray-500 my-1 inline-block">${{ product.price }}</span>
+      <span class="mx-4 text-gray-500 my-1 inline-block">
+        ${{ product.price }}
+      </span>
     </p>
     <div class="relative flex-grow">
-      <div class="absolute btn-plus">
+      <div
+        class="absolute flex justify-center items-center w-10 h-10 text-align-center border border-solid rounded-full btn-plus"
+      >
         <img src="../../assets/add.png" alt />
       </div>
     </div>
@@ -35,18 +45,10 @@ export default {
 
 <style>
 .btn-plus {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-  height: 40px;
-  text-align: center;
   right: 10px;
   bottom: -20px;
   background: #0f292d;
-  border: 1px solid #0e9ec2;
+  border-color: #0e9ec2;
   color: #0e9ec2;
-  box-sizing: border-box;
-  border-radius: 500px;
 }
 </style>
