2c125cad01e2d0ab08108fd82b11cbd5badf0826
FrozenIce0617 <prosdev087@gmail.com>
2020-04-03 12:09:18 +0200

    feat: added AddCart button

--- src/components/cards/ProductCard.vue
+++ src/components/cards/ProductCard.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="bg-secondary flex flex-row shadow-md rounded-lg h-24 mb-4">
-    <img
-      :src="product.image"
-      alt="Product image"
-      class="w-3/12 h-full object-cover rounded-l-lg"
-    />
+    <img :src="product.image" alt="Product image" class="w-3/12 h-full object-cover rounded-l-lg" />
     <p class="my-auto">
-      <span class="mx-4 text-gray-400 my-1 inline-block text-lg">
-        {{ product.name }}
-      </span>
+      <span class="mx-4 text-gray-400 my-1 inline-block text-lg">{{ product.name }}</span>
       <br />
-      <span class="mx-4 text-gray-500 my-1 inline-block">
-        ${{ product.price }}
-      </span>
+      <span class="mx-4 text-gray-500 my-1 inline-block">${{ product.price }}</span>
     </p>
+    <div class="relative flex-grow">
+      <div class="absolute btn-plus">
+        <img src="../../assets/add.png" alt />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,4 +33,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.btn-plus {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  right: 10px;
+  bottom: -20px;
+  background: #0f292d;
+  border: 1px solid #0e9ec2;
+  color: #0e9ec2;
+  box-sizing: border-box;
+  border-radius: 500px;
+}
+</style>
