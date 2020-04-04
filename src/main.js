300c0f1b06d5ef4305210c58396b74d4a1828e39
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-04 19:05:32 +0530

    chore: removed filters

--- src/main.js
+++ src/main.js
@@ -7,26 +7,6 @@ import './assets/tailwind.css';
 
 Vue.config.productionTip = false;
 
-Vue.filter('dashToSpace', function(value) {
-  if (!value) return '';
-  return value.toString().replace(/-/g, ' ');
-});
-
-Vue.filter('capitalize', function(value) {
-  if (!value) return '';
-  return value
-    .toString()
-    .split(' ')
-    .map((w, i) => {
-      if (i === 0 || w !== 'and') {
-        return w.charAt(0).toUpperCase() + w.slice(1);
-      } else {
-        return w;
-      }
-    })
-    .join(' ');
-});
-
 new Vue({
   router,
   store,
