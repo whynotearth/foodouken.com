83f76fba506cba1c75bb0f39601a3407e8984699
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-04 19:11:11 +0530

    refactor: productCard

--- src/components/cards/ProductCard.vue
+++ src/components/cards/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-on:click="addCartProduct(product)"
-    class="bg-secondary flex flex-row shadow-md rounded-lg h-24 mb-4"
+    class="bg-secondary flex flex-row shadow-md rounded-lg h-24 mb-4 cursor-pointer"
   >
     <img
       :src="product.image"
@@ -10,24 +10,23 @@
     />
     <p class="my-auto">
       <span class="mx-4 text-gray-400 my-1 inline-block text-lg">
-        {{ this.product.name }}
+        {{ product.name }}
       </span>
       <br />
       <span class="mx-4 text-gray-500 my-1 inline-block">
-        ${{ this.product.price }}
+        ${{ product.price }}
       </span>
     </p>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   name: 'ProductCard',
   props: ['product'],
   methods: {
-    addCartProduct(product) {
-      this.$store.dispatch('cart/addCartProduct', product);
-    }
+    ...mapActions('cart', ['addCartProduct'])
   }
 };
 </script>
