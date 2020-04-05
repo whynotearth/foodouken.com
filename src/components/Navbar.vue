cca7005941a016308419bb53a1034c9e43b1a82a
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-05 15:08:46 +0530

    fix: navigation bar z-index

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-white font-bold text-2xl bg-primary sticky top-0">
+  <div class="text-white font-bold text-2xl bg-primary sticky top-0 z-10">
     <div class="max-w-screen-xxl flex flex-row py-2 px-4 md:px-8 m-auto">
       <div class="w-full text-center">Bang Bang Bakery</div>
     </div>
