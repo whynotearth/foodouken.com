2176522cca62d261b8edc72e5cba3eef02e9f123
Isaiah Hunter <hunter.isaiah96@gmail.com>
2020-04-07 11:51:32 -0400

    fixed decimal issue

--- src/components/cart/Cart.vue
+++ src/components/cart/Cart.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="bg-secondary shadow-md rounded-lg mb-4 p-4 max-w-sm m-auto">
-    <cart-item
-      v-for="cartItem in cartItems"
-      :cart-item="cartItem"
-      :key="cartItem.product.id"
-    />
+    <cart-item v-for="cartItem in cartItems" :cart-item="cartItem" :key="cartItem.product.id" />
     <hr class="my-4" />
     <div class="grid grid-cols-2 gap-1">
-      <div><span class="text-gray-500 text-sm">Sub-Total</span></div>
+      <div>
+        <span class="text-gray-500 text-sm">Sub-Total</span>
+      </div>
       <div class="text-right">
-        <span class="text-gray-500 text-sm">
-          ${{ subTotal | formatPrice }}
-        </span>
+        <span class="text-gray-500 text-sm">${{ subTotal | formatPrice }}</span>
       </div>
 
-      <div><span class="text-gray-500 text-sm">Delivery Fee</span></div>
+      <div>
+        <span class="text-gray-500 text-sm">Delivery Fee</span>
+      </div>
       <div class="text-right">
         <span class="text-gray-500 text-sm">${{ deliveryFee }}</span>
       </div>
     </div>
     <div class="grid grid-cols-2 gap-1 mt-2">
-      <div><span class="text-gray-400 text-2xl font-bold">Total</span></div>
+      <div>
+        <span class="text-gray-400 text-2xl font-bold">Total</span>
+      </div>
       <div class="text-right">
-        <span class="text-gray-400 text-2xl font-bold">${{ total }}</span>
+        <span class="text-gray-400 text-2xl font-bold">${{ total | formatPrice }}</span>
       </div>
     </div>
   </div>
