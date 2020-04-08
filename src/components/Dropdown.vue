f940202c1c83f80478577e29b3c063f8cf2ccfcb
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-08 18:38:36 +0530

    feat: dropdown

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="mt-2 py-2 w-48 bg-dropdown rounded-lg shadow-xl"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown'
+};
+</script>
+
+<style></style>
