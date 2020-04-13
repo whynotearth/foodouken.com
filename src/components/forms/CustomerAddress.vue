807b0b6561974c34ac94ead13dd41f927e857438
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-14 02:55:28 +0530

    feat: implemented googleLocation

--- src/components/forms/CustomerAddress.vue
+++ src/components/forms/CustomerAddress.vue
@@ -1,48 +1,74 @@
 <template>
   <div class="w-full">
-    <div class="bg-secondary px-2 pt-4 pb-1 rounded-lg shadow">
-      <material-input
-        v-model="buildingName"
-        type="text"
-        label="Building Name"
-        placeholder="Building Name"
+    <div class="w-full bg-secondary rounded-lg shadow mb-2">
+      <RadioInput v-model="option" value="Use my location" class="p-5" />
+      <hr class="border-gray-700" />
+      <RadioInput
+        v-model="option"
+        value="Enter location manually"
+        class="p-5"
       />
-      <material-input
-        v-model="street"
-        type="text"
-        label="Street Address"
-        placeholder="Street name and number"
-        :error="$v.street.$dirty && !$v.street.required"
-      >
-        <span
-          v-if="$v.street.$dirty && !$v.street.required"
-          class="text-xs text-red-600"
-        >
-          Street address is required
-        </span>
-      </material-input>
-      <material-input
-        v-model="town"
-        type="text"
-        label="City"
-        placeholder="Town or City Area"
-        :error="$v.town.$dirty && !$v.town.required"
-      >
-        <span
-          v-if="$v.town.$dirty && !$v.town.required"
-          class="text-xs text-red-600"
-        >
-          Town/City is required
-        </span>
-      </material-input>
-      <material-input v-model="floor" type="text" label="Floor" />
-      <material-input v-model="apartment" type="text" label="Apartment #" />
-      <material-input v-model="parking" type="text" label="Where to park" />
     </div>
-    <span class="text-sm text-red-600" v-if="$v.$invalid" v-show="submitError">
-      You are missing required fields, please fill out "Street Address" and
-      "Town/City".
-    </span>
+    <div
+      v-if="option === 'Use my location'"
+      class="w-full bg-secondary rounded-lg shadow mb-2 p-5"
+    >
+      <a v-if="location.length > 0" :href="location" class="underline">
+        View my location
+      </a>
+      <p v-if="locationError.length > 0" class="text-red-600">
+        {{ locationError }}
+      </p>
+    </div>
+    <div v-else>
+      <div class="bg-secondary px-2 pt-4 pb-1 rounded-lg shadow">
+        <material-input
+          v-model="buildingName"
+          type="text"
+          label="Building Name"
+          placeholder="Building Name"
+        />
+        <material-input
+          v-model="street"
+          type="text"
+          label="Street Address"
+          placeholder="Street name and number"
+          :error="$v.street.$dirty && !$v.street.required"
+        >
+          <span
+            v-if="$v.street.$dirty && !$v.street.required"
+            class="text-xs text-red-600"
+          >
+            Street address is required
+          </span>
+        </material-input>
+        <material-input
+          v-model="town"
+          type="text"
+          label="City"
+          placeholder="Town or City Area"
+          :error="$v.town.$dirty && !$v.town.required"
+        >
+          <span
+            v-if="$v.town.$dirty && !$v.town.required"
+            class="text-xs text-red-600"
+          >
+            Town/City is required
+          </span>
+        </material-input>
+        <material-input v-model="floor" type="text" label="Floor" />
+        <material-input v-model="apartment" type="text" label="Apartment #" />
+        <material-input v-model="parking" type="text" label="Where to park" />
+      </div>
+      <span
+        class="text-sm text-red-600"
+        v-if="$v.$invalid"
+        v-show="submitError"
+      >
+        You are missing required fields, please fill out "Street Address" and
+        "Town/City".
+      </span>
+    </div>
     <CheckoutNavBar
       nextStepText="Set delivery time ►"
       @previousStep="decrementPage"
@@ -53,6 +79,7 @@
 
 <script>
 import MaterialInput from '@/components/inputs/MaterialInput.vue';
+import RadioInput from '@/components/inputs/RadioInput.vue';
 import CheckoutNavBar from '@/components/forms/CheckoutNavBar.vue';
 import { mapGetters, mapMutations, mapActions } from 'vuex';
 import { required } from 'vuelidate/lib/validators';
@@ -61,11 +88,13 @@ export default {
   name: 'CustomerAddress',
   components: {
     MaterialInput,
+    RadioInput,
     CheckoutNavBar
   },
   data() {
     return {
-      submitError: false
+      submitError: false,
+      locationError: ''
     };
   },
   validations: {
@@ -76,6 +105,11 @@ export default {
       required
     }
   },
+  created() {
+    if (this.option === 'Use my location') {
+      this.getCoordinates();
+    }
+  },
   computed: {
     ...mapGetters('form', [
       'getTown',
@@ -84,8 +118,18 @@ export default {
       'getParking',
       'getBuildingName',
       'getStreet',
+      'getAddressOption',
+      'getGoogleLocation',
       'page'
     ]),
+    option: {
+      get() {
+        return this.getAddressOption;
+      },
+      set(value) {
+        this.updateAddressOption(value);
+      }
+    },
     town: {
       get() {
         return this.getTown;
@@ -133,6 +177,14 @@ export default {
       set(value) {
         this.updateApartment(value);
       }
+    },
+    location: {
+      get() {
+        return this.getGoogleLocation;
+      },
+      set(value) {
+        this.updateGoogleLocation(value);
+      }
     }
   },
   methods: {
@@ -144,24 +196,44 @@ export default {
       'updateBuildingName',
       'updateParking',
       'updateApartment',
+      'updateAddressOption',
+      'updateGoogleLocation',
       'pageChange'
     ]),
     submit() {
-      this.$v.$touch();
-      if (this.$v.$invalid) {
-        this.submitError = true;
-      } else {
-        this.register().then(() => {
-          this.pageChange(3);
-        });
+      if (this.option !== 'Use my location') {
+        this.$v.$touch();
+        if (this.$v.$invalid) {
+          this.submitError = true;
+          return false;
+        }
       }
+      this.register().then(() => {
+        this.pageChange(3);
+      });
     },
     decrementPage() {
       const pageToGo = this.page - 1;
       this.pageChange(pageToGo);
+    },
+    getCoordinates() {
+      navigator.geolocation.getCurrentPosition(this.success, this.error);
+    },
+    success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      this.location = `http://maps.google.com/maps?q=${latitude},${longitude}&z=13`;
+    },
+    error() {
+      this.locationError = 'Unable to retrieve your location';
+    }
+  },
+  watch: {
+    option: function(val) {
+      if (val === 'Use my location') {
+        this.getCoordinates();
+      }
     }
   }
 };
 </script>
-
-<style></style>
