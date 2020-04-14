9a7917bfba7b7696fb99a42cdca1531aa20d54fc
spark <parkyngj@gmail.com>
2020-04-13 20:54:09 -0700

    feat: forms fade in when switching pages

--- src/components/forms/CheckoutForm.vue
+++ src/components/forms/CheckoutForm.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="max-w-sm m-auto">
     <checkout-stepper v-if="page < 6" />
-    <customer-info v-if="page === 1" />
-    <customer-address v-if="page === 2" />
-    <delivery-time v-if="page === 3" />
-    <payment-method v-if="page === 4" />
-    <review-order v-if="page === 5" />
-    <thank-you v-if="page === 6" />
+    <transition name="fade">
+      <customer-info v-if="page === 1" />
+      <customer-address v-if="page === 2" />
+      <delivery-time v-if="page === 3" />
+      <payment-method v-if="page === 4" />
+      <review-order v-if="page === 5" />
+      <thank-you v-if="page === 6" />
+    </transition>
   </div>
 </template>
 
