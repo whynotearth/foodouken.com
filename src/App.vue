02add217d696174712a9e32109080653c03eff7c
A_step_below_classy <kmb_x@hotmail.com>
2020-04-16 15:25:08 +0700

    Feat: Added Credit Footer

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,9 @@
     <Spinner class="min-h-screen" v-if="getLoading" />
     <transition name="fade">
       <div v-show="!getLoading">
-        <router-view
-          class="md:px-8 pb-8 px-4 max-w-screen-xxl mx-auto min-h-screen"
-        />
+        <router-view class="md:px-8 pb-8 px-4 max-w-screen-xxl mx-auto min-h-screen" />
         <Footer />
+        <CreditFooter />
       </div>
     </transition>
   </div>
@@ -15,12 +14,14 @@
 <script>
 import Footer from '@/components/Footer.vue';
 import Spinner from '@/components/Spinner.vue';
+import CreditFooter from '@/components/CreditFooter.vue';
 import { mapGetters } from 'vuex';
 
 export default {
   components: {
     Footer,
-    Spinner
+    Spinner,
+    CreditFooter
   },
   computed: {
     ...mapGetters('home', ['getLoading'])
---
+++ src/components/CreditFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center justify-center text-white align-items-center w-full h-8">
+    <div id="credit">
+      <span>
+        ⚡by
+        <a class="underline" href="foodouken.com">Foodouken.com</a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreditFooter'
+};
+</script>
+
+<style scoped>
+#credit {
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 16px;
+  color: #ffffff 38%;
+}
+</style>
