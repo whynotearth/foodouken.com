c3bba41c6a3bb1750633add636751cacf3de343b
spark <parkyngj@gmail.com>
2020-04-18 17:16:27 -0700

    fix: progress text in progress circle properly centered

--- src/components/forms/CheckoutStepper.vue
+++ src/components/forms/CheckoutStepper.vue
@@ -2,7 +2,7 @@
   <div
     class="max-w-sm m-auto bg-footer shadow-md mb-4 flex items-center sticky lg:relative inset-x-0 top-0 z-10"
   >
-    <div>
+    <div class="relative">
       <svg class="w-20 h-20 text-inactive stroke-current">
         <circle :cx="centerX" :cy="centerY" :r="radius" fill="transparent" />
         <circle
@@ -13,16 +13,12 @@
           fill="transparent"
           :stroke-dasharray="strokeDashArray"
         />
-        <text
-          x="50%"
-          y="50%"
-          dominant-baseline="middle"
-          text-anchor="middle"
-          class="text-white text-sm fill-current stroke-0"
-        >
-          {{ pageProgress }}
-        </text>
       </svg>
+      <div
+        class="progress-ring-inner text-white text-sm absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+        {{ pageProgress }}
+      </div>
     </div>
     <div class="flex-grow text-right leading-tight p-4">
       <h3 class="text-xl font-bold">{{ pageHeader(page) }}</h3>
@@ -108,10 +104,5 @@ svg {
 
 svg .progress-bar {
   transition: stroke-dasharray 0.5s ease-in-out;
-}
-
-svg text {
-  transform: rotate(90deg);
-  transform-origin: 50% 50%;
 }
 </style>
