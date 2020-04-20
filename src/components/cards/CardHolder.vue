1c5514941b5eab0368b0fb83084e46a881a455e0
A_step_below_classy <kmb_x@hotmail.com>
2020-04-20 11:17:09 +0700

    Responsive upgrade by adding some grid options and maintaining image aspect ratio

--- src/components/cards/CardHolder.vue
+++ src/components/cards/CardHolder.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="grid lg:grid-cols-2 gap-6">
-    <product-card
-      :product="product"
-      v-for="product in getProducts"
-      :key="product.id"
-    />
+  <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols 2 lg:grid-cols-2 gap-6">
+    <product-card :product="product" v-for="product in getProducts" :key="product.id" />
   </div>
 </template>
 
