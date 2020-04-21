c341480e798b482e652c338c2ddff5e8edeab751
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-22 01:12:28 +0530

    chore: lintfix

--- src/components/CreditFooter.vue
+++ src/components/CreditFooter.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex justify-center text-white w-full h-12 text-xs leading-4 opacity-38">
+  <div
+    class="flex justify-center text-white w-full h-12 text-xs leading-4 opacity-38"
+  >
     <div>
       <span>
         ⚡by
