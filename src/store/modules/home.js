45a9a8157e3822e94a27d0cfe8f903348bfac3cc
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-21 22:44:58 +0530

    feat: homepage data dynamic

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -3,11 +3,17 @@ import { httpClient } from '@/services/httpClient';
 // initial state
 const state = {
   loading: true,
+  home: {
+    custom: {}
+  },
   tenants: []
 };
 
 // getters
 const getters = {
+  getHomeData(state) {
+    return state.home;
+  },
   getTenants(state) {
     return state.tenants;
   },
@@ -19,6 +25,23 @@ const getters = {
 // actions
 const actions = {
   fetchHomeData({ commit }) {
+    return new Promise((resolve, reject) => {
+      httpClient
+        .get(`/pages/slug/foodouken/foodouken-cambodia-food-delivery`)
+        .then(
+          response => {
+            commit('loadHomeData', response.data);
+            commit('changeHomeLoading', false);
+            resolve(response.data);
+          },
+          error => {
+            commit('changeHomeLoading', false);
+            reject(error);
+          }
+        );
+    });
+  },
+  fetchTenants({ commit }) {
     return new Promise((resolve, reject) => {
       httpClient.get(`/companies/foodouken/tenants`).then(
         response => {
@@ -37,6 +60,9 @@ const actions = {
 
 // mutations
 const mutations = {
+  loadHomeData(state, payload) {
+    state.home = payload;
+  },
   loadTenants(state, payload) {
     state.tenants = payload;
   },
