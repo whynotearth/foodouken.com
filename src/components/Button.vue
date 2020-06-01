da7be28d3bf5069dcaad1b102eb128631d507064
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-06-01 14:52:11 +0300

    some changes on typography.css opacity.js and Button.vue what Sign up process needs

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,13 +1,16 @@
 <template>
   <component
     :is="type"
-    class="ripple cursor-pointer text-white text-sm font-semibold uppercase p-4 bg-button"
-    :class="width"
+    class="cursor-pointer text-white text-sm font-semibold uppercase p-4"
+    :class="[isRipple ? 'ripple' : '', width, buttonBg]"
     @click="$emit('clicked')"
     :href="href"
     :to="to"
   >
     <div class="flex flex-row items-center">
+      <div class="text-left">
+        <slot name="icon"></slot>
+      </div>
       <div
         class="text-left"
         :class="[titleLeft || title ? 'flex-1' : 'hidden']"
@@ -52,6 +55,14 @@ export default {
     width: {
       type: String,
       default: 'w-full'
+    },
+    buttonBg: {
+      type: String,
+      default: 'bg-button'
+    },
+    isRipple: {
+      type: Boolean,
+      default: true
     }
   },
   computed: {
@@ -73,10 +84,12 @@ export default {
   background-position: center;
   transition: background 0.4s;
 }
+
 .ripple:hover {
   background: #47a7f5 radial-gradient(circle, transparent 1%, #47a7f5 1%)
     center/15000%;
 }
+
 .ripple:active {
   background-color: #6eb9f7;
   background-size: 100%;
