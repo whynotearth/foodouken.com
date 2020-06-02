2942c2f9513f49f335842ef8d2c46ab036872bc3
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-02 11:14:17 +0530

    feat: added VariantManager

---
+++ src/components/menu/VariantManager.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="mx-4">
+    <div class="w-full my-6">
+      <span class="tg-h3-mobile text-white text-opacity-54 inline-block">
+        {{ title }}
+      </span>
+      <img
+        :src="down"
+        alt="down arrow"
+        class="inline-block py-3 float-right pointer-events-none"
+        :class="{ 'transform rotate-180': showVariants }"
+      />
+    </div>
+    <div class="shadow-1dp bg-secondary rounded-lg">
+      <div v-if="showVariants" class="pt-4">
+        <div v-for="(variant, index) in variants" :key="index" class="flex">
+          <MaterialInput
+            v-model="variant.name"
+            :label="`Option ${index + 1}`"
+            class="w-2/3 mx-2"
+          />
+          <MaterialInput
+            v-model="variant.price"
+            label="Price"
+            class="w-1/3 mx-2"
+          />
+        </div>
+        <hr class="border-white border-opacity-12" />
+      </div>
+      <Button
+        @clicked="addVariant"
+        :title="buttonTitle"
+        :isRipple="false"
+        buttonBg="bg-transparent"
+        class="text-button"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import MaterialInput from '@/components/inputs/MaterialInput';
+import Button from '@/components/Button';
+import down from '@/assets/down.png';
+
+export default {
+  name: 'VariantManager',
+  components: { MaterialInput, Button },
+  model: {
+    prop: 'variants',
+    event: 'updateVariants'
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Choose please!'
+    },
+    buttonTitle: {
+      type: String,
+      default: 'Add Variation'
+    },
+    variants: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      down,
+      newVariant: {
+        name: '',
+        price: ''
+      }
+    };
+  },
+  computed: {
+    showVariants() {
+      return this.variants.length > 0 ? true : false;
+    }
+  },
+  methods: {
+    addVariant() {
+      this.$emit('updateVariants', [...this.variants, this.newVariant]);
+    }
+  }
+};
+</script>
