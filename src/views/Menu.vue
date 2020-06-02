0cb5de980d8c8ab3dfdfe2621c305eab1017cc90
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-02 11:11:49 +0530

    feat: added menu.vue

---
+++ src/views/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mx-auto">
+    <BaseAppBarHeader
+      title="Bagels & Bread"
+      :toLink="{ name: 'MenuItemList' }"
+    />
+    <transition name="fade" mode="out-in">
+      <router-view class="container mx-auto"></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import BaseAppBarHeader from '@/components/BaseAppBarHeader';
+export default {
+  name: 'Menu',
+  components: {
+    BaseAppBarHeader
+  }
+};
+</script>
