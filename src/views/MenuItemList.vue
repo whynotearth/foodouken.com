e9d45eee47e8bd274ea1fc5bc7fd3af60d22a2e8
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-02 11:13:48 +0530

    feat: added MenuItemList

---
+++ src/views/MenuItemList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-2 ">
+    <MenuItem v-for="item in getMenuItems" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script>
+import MenuItem from '@/components/menu/MenuItem';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'MenuItemList',
+  components: {
+    MenuItem
+  },
+  computed: {
+    ...mapGetters('menu', ['getMenuItems'])
+  }
+};
+</script>
