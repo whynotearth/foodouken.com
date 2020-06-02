c1a4ebea484ffbee5fcf4f991d26a056d356574c
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-02 12:12:03 +0530

    feat: added new_item button on BaseAppBarHeader

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="mx-auto">
-    <BaseAppBarHeader
-      title="Bagels & Bread"
-      :toLink="{ name: 'MenuItemList' }"
-    />
+    <BaseAppBarHeader title="Bagels & Bread" :toLink="{ name: 'MenuItemList' }">
+      <template #menu>
+        <div class="flex-grow inline-block text-right">
+          <router-link :to="{ name: 'MenuItemsAdd' }" class="relative">
+            <NewItemIcon class="float-right w-5 h-5 cursor-pointer" />
+          </router-link>
+        </div>
+      </template>
+    </BaseAppBarHeader>
     <transition name="fade" mode="out-in">
       <router-view class="container mx-auto"></router-view>
     </transition>
@@ -12,10 +17,12 @@
 
 <script>
 import BaseAppBarHeader from '@/components/BaseAppBarHeader';
+import NewItemIcon from '@/assets/new_item.svg';
 export default {
   name: 'Menu',
   components: {
-    BaseAppBarHeader
+    BaseAppBarHeader,
+    NewItemIcon
   }
 };
 </script>
