5b31a97d0255b9aed5ee5dd956b195c9a89de332
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-04 11:56:19 +0530

    feat: added sold out and click to edit item

--- src/views/MenuItemList.vue
+++ src/views/MenuItemList.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-3 gap-2 ">
-    <MenuItem v-for="item in getMenuItems" :key="item.id" :item="item" />
+    <MenuItem
+      v-for="item in getMenuItems"
+      :key="item.id"
+      :item="item"
+      :options="menuItemOptions(item)"
+      @clicked="editItem(item)"
+    />
   </div>
 </template>
 
 <script>
 import MenuItem from '@/components/menu/MenuItem';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 
 export default {
   name: 'MenuItemList',
@@ -15,6 +21,31 @@ export default {
   },
   computed: {
     ...mapGetters('menu', ['getMenuItems'])
+  },
+  methods: {
+    ...mapMutations('menu', ['updateItem']),
+    menuItemOptions(item) {
+      return [
+        {
+          name: 'Sold out',
+          action: () => this.sellOutItem(item)
+        },
+        {
+          name: 'Delete',
+          action: () => this.deleteItem(item)
+        }
+      ];
+    },
+    sellOutItem(item) {
+      console.log(item.name, 'is sold out');
+    },
+    deleteItem(item) {
+      console.log(item.name, 'is deleted');
+    },
+    editItem(item) {
+      this.updateItem(item);
+      this.$router.push({ name: 'MenuItemsAdd' });
+    }
   }
 };
 </script>
