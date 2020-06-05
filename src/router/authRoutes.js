9da31e24797d9a365234a982fca201b8e0f0ba36
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-06-05 09:53:31 +0300

    dashboard routes file created

---
+++ src/router/authRoutes.js
@@ -0,0 +1,29 @@
+const authRoutes = [
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    component: () => import('@/views/Welcome.vue'),
+    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
+  },
+  {
+    path: '/auth/sign-up/:step',
+    name: 'SignUp',
+    props: true,
+    component: () => import('@/views/SignUp.vue'),
+    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
+  },
+  {
+    path: '/auth/login',
+    name: 'LogIn',
+    component: () => import('@/views/LogIn.vue'),
+    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
+  },
+  {
+    path: '/auth/sign-up/success/:slug',
+    name: 'SignUpSuccess',
+    component: () => import('@/components/auth/SignUpSuccess.vue'),
+    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
+  },
+];
+
+module.exports = authRoutes;
--- src/router/index.js
+++ src/router/index.js
@@ -35,31 +35,6 @@ const routes = [
     meta: { layout: () => import('@/layouts/TenantLayout.vue') }
   },
   {
-    path: '/welcome',
-    name: 'Welcome',
-    component: () => import('@/views/Welcome.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
-    path: '/auth/sign-up/:step',
-    name: 'SignUp',
-    props: true,
-    component: () => import('@/views/SignUp.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
-    path: '/auth/login',
-    name: 'LogIn',
-    component: () => import('@/views/LogIn.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
-    path: '/auth/sign-up/success/:slug',
-    name: 'SignUpSuccess',
-    component: () => import('@/components/auth/SignUpSuccess.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
     path: '/*',
     redirect: '/'
   }
