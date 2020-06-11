782126b7ab52d51eceed1d0e3dcc74ee3f3908dc
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-11 18:13:43 +0530

    feat: new modules

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import tenantMenuRoutes from './tenantMenuRoutes';
-import { authRoutes } from './authRoutes';
 import store from '../store';
+
+import { customerViewRoutes } from './customerViewRoutes';
+import { tenantCMSRoutes } from './tenantCMSRoutes';
+import { authRoutes } from './authRoutes';
+import { settingRoutes } from './settingRoutes';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home.vue'),
-    meta: { layout: () => import('@/layouts/Default.vue') }
-  },
-  {
-    path: '/shop/:slug/:orderingStepSlug?',
-    name: 'Shop',
-    component: () => import('@/views/Shop.vue'),
-    meta: { layout: () => import('@/layouts/ShopLayout.vue') }
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: () => import('@/views/Settings.vue'),
-    meta: {
-      layout: () => import('@/layouts/TenantLayout.vue'),
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/settings/account',
-    name: 'Account',
-    component: () => import('@/views/Account.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  {
-    path: '/settings/business',
-    name: 'Business',
-    component: () => import('@/views/Business.vue'),
-    meta: { layout: () => import('@/layouts/TenantLayout.vue') }
-  },
-  ...tenantMenuRoutes,
+  ...customerViewRoutes,
+  ...settingRoutes,
+  ...tenantCMSRoutes,
   ...authRoutes,
   {
     path: '/*',
