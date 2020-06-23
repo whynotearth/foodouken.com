3802184197052b7597f1bde18d1cc37d21f7f856
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-23 15:37:33 +0530

    feat: added overlay to App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,32 @@
     <component :is="this.$route.meta.layout || 'div'">
       <router-view />
     </component>
+    <transition name="fade">
+      <div
+        v-if="overlayModel.title || overlayModel.message"
+        class="w-full h-full fixed block top-0
+        left-0 z-110"
+      >
+        <BaseOverlaySuccess
+          :title="overlayModel.title"
+          :message="overlayModel.message"
+        />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import BaseOverlaySuccess from '@/components/BaseOverlaySuccess.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { BaseOverlaySuccess },
+  computed: {
+    overlayModel() {
+      return this.$store.getters['overlay/model'];
+    }
+  }
 };
 </script>
 
