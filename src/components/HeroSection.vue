4bc1464d3179c8dad0f187f9cb1cdb294bf87ac5
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-26 07:37:31 +0530

    fix: keys in heroSection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="text-center">
-    <div class="mt-8" v-if="heroData.custom">
+    <div class="mt-8">
       <img
-        :src="heroData.custom.logo"
+        :src="heroData.logo"
         alt="Logo image"
         class="h-48 w-48 m-auto rounded-full"
       />
     </div>
     <h1 class="text-white md:text-6xl text-5xl font-bold">
-      {{ heroData.title }}
+      {{ heroData.name }}
     </h1>
     <h2 class="md:text-5xl text-3xl text-gray-500 font-bold mb-8">Delivery</h2>
     <p class="text-gray-500 mt-2 whitespace-pre-wrap mx-auto max-w-lg">
