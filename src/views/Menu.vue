136c425f3f6cda57d7da5ad10faaf3327a5548b1
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-29 20:40:28 +0530

    feat: removed tenant check logic from created in Menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -6,22 +6,6 @@
 
 <script>
 export default {
-  name: 'Menu',
-  created() {
-    this.$store
-      .dispatch('tenant/userOwnsTenant', this.$route.params.tenantSlug)
-      .then(result => {
-        if (result) {
-          this.$router.push({ name: 'MenuCategoryList' });
-        } else {
-          this.$router.push({ name: 'Account' });
-          alert('You do not have permission to access this tenant!');
-        }
-      })
-      .catch(error => {
-        this.$router.push({ name: 'Account' });
-        throw new Error(error);
-      });
-  }
+  name: 'Menu'
 };
 </script>
