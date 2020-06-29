7591ca0c67876aed20a5729790cfe90b6aabf50f
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-29 19:54:26 +0530

    feat: added transition and userOwnsTenant check

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,40 +1,27 @@
 <template>
-  <div class="mx-auto">
-    <BaseAppBarHeader :title="appBar.title" :toLink="appBar.backRoute">
-      <template #menu v-if="appBar.newItem">
-        <div class="flex-grow inline-block text-right">
-          <router-link :to="appBar.newItem" class="relative">
-            <NewItemIcon class="float-right w-5 h-5 my-auto cursor-pointer" />
-          </router-link>
-        </div>
-      </template>
-    </BaseAppBarHeader>
-    <transition name="fade" mode="out-in">
-      <router-view class="container mx-auto"></router-view>
-    </transition>
-  </div>
+  <transition name="fade" mode="out-in">
+    <router-view />
+  </transition>
 </template>
 
 <script>
-import BaseAppBarHeader from '@/components/BaseAppBarHeader';
-import NewItemIcon from '@/assets/new_item.svg';
-
 export default {
   name: 'Menu',
-  components: {
-    BaseAppBarHeader,
-    NewItemIcon
-  },
-  computed: {
-    appBar() {
-      let route = this.$route,
-        appBarData = {
-          title: 'Navbar',
-          backRoute: { name: 'Home' }
-        };
-      if (route.meta.appBar) appBarData = route.meta.appBar;
-      return appBarData;
-    }
+  created() {
+    this.$store
+      .dispatch('tenant/userOwnsTenant', this.$route.params.tenantSlug)
+      .then(result => {
+        if (result) {
+          this.$router.push({ name: 'MenuCategoryList' });
+        } else {
+          this.$router.push({ name: 'Account' });
+          alert('You do not have permission to access this tenant!');
+        }
+      })
+      .catch(error => {
+        this.$router.push({ name: 'Account' });
+        throw new Error(error);
+      });
   }
 };
 </script>
