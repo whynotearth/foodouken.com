2d745a501c059f9961050c256ccc8c64a2494872
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-29 19:59:13 +0530

    feat: removed prefixRoutes func

--- src/router/index.js
+++ src/router/index.js
@@ -9,18 +9,10 @@ import { settingRoutes } from './settingRoutes';
 
 Vue.use(VueRouter);
 
-// If you use prefixRoutes function, make sure that your child routes start with a '/'
-function prefixRoutes(prefix, routes) {
-  return routes.map(route => {
-    route.path = prefix + '' + route.path;
-    return route;
-  });
-}
-
 const routes = [
   ...customerViewRoutes,
   ...settingRoutes,
-  ...prefixRoutes('/tenant/:tenantSlug/menu', [...tenantCMSRoutes]),
+  ...tenantCMSRoutes,
   ...authRoutes,
   {
     path: '/*',
