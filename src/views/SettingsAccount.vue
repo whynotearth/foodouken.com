5c1484a81558b7ab92b446fe167c5adc123cb94e
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-29 19:04:41 +0530

    feat: SettingsAccount Page

--- src/views/SettingsAccount.vue
+++ src/views/SettingsAccount.vue
@@ -1,9 +1,69 @@
 <template>
-  <div></div>
+  <div class="p-4">
+    <h3 class="tg-h3-mobile text-white text-opacity-84">Business Management</h3>
+    <span v-if="apiError" class="my-8 mx-4 text-sm text-red-600">
+      {{ apiError }}
+    </span>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 my-4">
+      <div
+        v-for="(tenant, index) in tenants"
+        :key="index"
+        class="shadow-1dp bg-secondary rounded-lg p-2 max-w-sm"
+      >
+        <MenuItem
+          :name="tenant.name"
+          :image="tenant.logoUrl"
+          @clicked="manageTenant(tenant.slug)"
+        >
+          <template #subHeading>
+            Foodouken
+          </template>
+        </MenuItem>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import MenuItem from '@/components/menu/MenuItem';
+import { mapActions } from 'vuex';
 
-<style></style>
+export default {
+  name: 'SettingsAccount',
+  components: { MenuItem },
+  data() {
+    return {
+      apiError: '',
+      tenants: []
+    };
+  },
+  created() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.tenants = [];
+    this.apiError = '';
+  },
+  methods: {
+    ...mapActions('tenant', ['fetchUserTenants']),
+    init() {
+      this.fetchUserTenants()
+        .then(tenants => {
+          this.tenants = tenants;
+        })
+        .catch(error => {
+          this.apiError = error.response.data.title
+            ? error.response.data.title
+            : 'Something went wrong, try again.';
+          throw error;
+        });
+    },
+    manageTenant(tenantSlug) {
+      this.$router.push({
+        name: 'MenuCategoryList',
+        params: { tenantSlug: tenantSlug }
+      });
+    }
+  }
+};
+</script>
