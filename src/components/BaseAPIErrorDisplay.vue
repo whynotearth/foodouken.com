9b8813a566c6ca4ebd52f4505c85e2f4ce265a74
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-30 17:39:29 +0530

    feat: added BaseAPIErrorDisplay component

---
+++ src/components/BaseAPIErrorDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="error" class="text-sm text-error">
+    <span v-if="error.title">
+      {{ error.title }}
+    </span>
+    <span v-else-if="error.message">{{ error.message }}</span>
+    <span v-else>{{ error }}</span>
+    <template v-if="error.errors">
+      <div v-for="(error, key) in error.errors" :key="key">
+        <p v-for="(detail, key) in error" :key="key">{{ detail }}</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseAPIErrorDisplay',
+  props: {
+    error: [String, Object]
+  }
+};
+</script>
