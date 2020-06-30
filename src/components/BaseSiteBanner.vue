7ba03aa810f024e73d45fdc10dfc40a57a1949fe
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-30 14:26:33 +0530

    chore: lintfixes

--- src/components/BaseSiteBanner.vue
+++ src/components/BaseSiteBanner.vue
@@ -5,10 +5,7 @@
       backdrop ? 'fixed h-screen w-screen bg-black bg-opacity-54' : 'sticky'
     "
   >
-    <div
-      class="p-2 text-center font-bold text-2xl"
-      :class="background"
-    >
+    <div class="p-2 text-center font-bold text-2xl" :class="background">
       <slot>Site Banner</slot>
     </div>
   </div>
