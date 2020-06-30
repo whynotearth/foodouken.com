12d06f85ebabb5663b6dfeafcdc00e32f773bb5a
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-30 17:28:08 +0530

    chore: update footer credit link

--- src/components/CreditFooter.vue
+++ src/components/CreditFooter.vue
@@ -9,7 +9,7 @@
         <a
           class="underline"
           target="_blank"
-          href="http://foodouken-8006819.hs-sites.com/foodouken-open-beta-restaurant-sign-up"
+          href="https://hub.foodouken.com/foodouken-open-beta-restaurant-sign-up"
           rel="noopener"
         >
           Foodouken.com
