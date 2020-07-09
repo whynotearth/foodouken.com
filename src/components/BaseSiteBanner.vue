8ce23391fe17d0cb7990bf1e3d31b5861f756b06
Joe Elia <mrjoeelia@gmail.com>
2020-07-09 19:14:33 -0400

    Created a small cookie utility and added X svg button to dismiss banner

--- src/components/BaseSiteBanner.vue
+++ src/components/BaseSiteBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div
+  <div v-if="showBanner"
     class="top-0 w-full z-100"
     :class="
       backdrop ? 'fixed h-screen w-screen bg-black bg-opacity-54' : 'sticky'
@@ -11,10 +11,12 @@
     >
       <slot>Site Banner</slot>
     </div>
+    <svg @click="setBannerCookie()" class="absolute top-0 right-0 h-8 w-8 cursor-pointer" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
   </div>
 </template>
 
 <script>
+import cookie from '@/utils/cookie'
 export default {
   name: 'BaseSiteBanner',
   props: {
@@ -25,6 +27,27 @@ export default {
     backdrop: {
       type: Boolean,
       default: false
+    }
+  },
+  data: function () {
+    return {
+      showBanner: false
+      }
+  },
+  beforeMount () {
+    var bannerCookie = cookie.getCookie("banner");
+    //if bannerCookie === 1 that means user has seen the banner and dismissed it
+    if (bannerCookie == 1){
+      this.showBanner = false;
+    } else {
+      this.showBanner = true;
+    }
+  },
+  methods: {
+    setBannerCookie: function () {
+      this.showBanner= false;
+      //set cookie with name 'banner'. Set value to 1 which means true. Set expiration to 7 days.
+      cookie.setCookie("banner",1,7);
     }
   }
 };
