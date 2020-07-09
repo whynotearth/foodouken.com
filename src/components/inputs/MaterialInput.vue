07a463ff100924297922eb7ababb4c5812c289d5
Joe Elia <mrjoeelia@gmail.com>
2020-07-09 16:20:41 -0400

    Remove leading and trailing white spaces on all input fields

--- src/components/inputs/MaterialInput.vue
+++ src/components/inputs/MaterialInput.vue
@@ -12,7 +12,7 @@
       :type="type"
       min="0"
       :step="step"
-      :value="value"
+      :value="value | trim"
       @blur="$emit('input', $event.target.value)"
       :pattern="pattern"
       :placeholder="placeholder || label"
@@ -65,6 +65,11 @@ export default {
     },
     labelBg: {
       type: String
+    }
+  },
+  filters: {
+    trim: function(string) {
+      return string.trim()
     }
   }
 };
