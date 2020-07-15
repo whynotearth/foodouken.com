dd59e4803f643b1b29480252f14071f12bf83b22
giteshk11 <writemail2gitesh@gmail.com>
2020-07-15 23:44:12 +0530

    login and avatar added. refs #283

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -13,6 +13,16 @@
           </a>
           ✊
         </BaseSiteBanner>
+        <div class="flex justify-end cursor-pointer mr-3">
+          <PlaceHolderAvatar v-if="isAuthenticated" @click="performRedirect" />
+          <span
+            class="text-sm md:text-lg login-text"
+            v-else
+            @click="performRedirect"
+          >
+            Login
+          </span>
+        </div>
         <router-view
           class="md:px-8 pb-8 px-4 max-w-screen-xxl mx-auto min-h-screen"
         />
@@ -24,16 +34,51 @@
 <script>
 import Spinner from '@/components/Spinner.vue';
 import BaseSiteBanner from '@/components/BaseSiteBanner.vue';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
+import PlaceHolderAvatar from '@/assets/placeholder_avatar.svg';
 
 export default {
   name: 'DefaultLayout',
   components: {
     Spinner,
-    BaseSiteBanner
+    BaseSiteBanner,
+    PlaceHolderAvatar
+  },
+  data() {
+    return {
+      isAuthenticated: false,
+      apiError: ''
+    };
   },
   computed: {
     ...mapGetters('home', ['getLoading'])
+  },
+  created() {
+    this.ping()
+      .then(response => {
+        if (response.isAuthenticated) {
+          this.isAuthenticated = response.isAuthenticated;
+        }
+      })
+      .catch(error => {
+        this.isAuthenticated = false;
+        this.apiError = error.response.data.title
+          ? error.response.data.title
+          : 'Something went wrong, try again.';
+        throw error;
+      });
+  },
+  methods: {
+    ...mapActions('auth', ['ping']),
+    performRedirect() {
+      this.$router.push({ name: this.isAuthenticated ? 'Account' : 'LogIn' });
+    }
   }
 };
 </script>
+
+<style scoped>
+.login-text {
+  color: #0e9ec2;
+}
+</style>
